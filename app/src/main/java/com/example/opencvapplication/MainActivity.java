eef3ff1c9628e52cb5a3d4225a7c8c7ac01f6554
SakshiBinthariyaAndroid <sakshi.binthariya@neosoftmail.com>
2022-08-17 17:36:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/opencvapplication/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.opencvapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (OpenCVLoader.initDebug()) {
+
+            Toast.makeText(this, "OpenCv configured successfully", Toast.LENGTH_SHORT).show();
+
+
+        } else {
+            Toast.makeText(this, "OpenCv doesn’t configured successfully", Toast.LENGTH_SHORT).show();
+
+
+        }
+
+    }
+}
